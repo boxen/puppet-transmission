4608f66ddf2a6c85c959003fff2251ea6aad3563
Nahuel Scotti <nahuel.scotti@gmail.com>
2013-10-07 01:56:19 +0200

    init

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class transmission($version='2.82') {
+  package { 'transmission':
+    provider => 'appdmg',
+    source   => "http://download.transmissionbt.com/files/Transmission-${version}.dmg"
+  }
 }
--- spec/classes/template_spec.rb
+++ spec/classes/template_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
-# Rename this file to classname_spec.rb
-# Check other boxen modules for examples
-# or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'transmission' do
+
+  version = '2.82'
+
   it do
-    should contain_anchor('Hello_World')
+    should contain_package("transmission").with({
+        :provider => 'appdmg',
+        :source   => "http://download.transmissionbt.com/files/Transmission-#{version}.dmg"
+    })
   end
 end
