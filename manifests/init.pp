bfede0554b4fdb0a5188e31d0c97f98d96b7bd73
Rolf van de Krol <info@rolfvandekrol.nl>
2015-02-21 18:26:01 +0100

    Bump to 2.84 and update the URL to use the new CDN that Transmission uses.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class transmission {
   package { 'transmission':
     provider   => 'appdmg',
-    source => 'http://download.transmissionbt.com/files/Transmission-2.82.dmg'
+    source => 'https://transmission.cachefly.net/Transmission-2.84.dmg'
   }
 }
